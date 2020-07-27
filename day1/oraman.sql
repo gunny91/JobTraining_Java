17192e2a3d7a1ebbc9a7acb023a437ef80f62cce
gunny91 <36052735+gunny91@users.noreply.github.com>
2020-07-27 15:44:50 +0900

    Update oraman.sql

--- day1/oraman.sql
+++ day1/oraman.sql
@@ -7,8 +7,29 @@
 -- member : ID, name, age, entry date
 
 create table members01(
-    id varchar2(15), -- character is using varchar2(15-> ÀÚ¸®¼ö)
+    id varchar2(15), -- character is using varchar2(15-> ìë¦¬ìˆ˜)
     name varchar2(100),
     age number,
     hire_date date    
+);
+
+--default value
+--sysdate: current time
+create table members02(
+    id varchar2(15),
+    name varchar2(100),
+    age number default 10,
+    hire_date date default sysdate 
+);
+
+-- primaray key: default key, must put and unique
+-- enhance the searching speed, it generate the key by itself.
+
+-- not null : must be entered colum!
+create table members03(
+    id varchar2(15) primary key,
+    name varchar2(100) not null,
+    age number default 10,
+    hire_date date default sysdate
+    
 );
