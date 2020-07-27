7bf50e743da2074eb8b64bfb047a8f7f846f5d95
Geonhee Cho <gunny91@iastate.edu>
2020-07-27 15:21:53 +0900

    oracle make table

---
+++ day1/oraman.sql
@@ -0,0 +1,14 @@
+--table name should be multiple
+--Create table name(
+    --colum_name1 type1,
+    --colum_name1 type2,
+--  ...
+-- );
+-- member : ID, name, age, entry date
+
+create table members01(
+    id varchar2(15), -- character is using varchar2(15-> ÀÚ¸®¼ö)
+    name varchar2(100),
+    age number,
+    hire_date date    
+);
