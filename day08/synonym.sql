4a8649395dab0f73d65fe407446bfbe4892fc429
Geonhee Cho <gunny91@iastate.edu>
2020-08-05 16:42:17 +0900

    quota practice get rid of user and create role

--- day08/synonym.sql
+++ day08/synonym.sql
@@ -11,3 +11,24 @@ select * from bigbang;
 --my books is synonym of bigbang.books
 create synonym mybook from bigbang.books;
 select * from mybook;
+
+
+--Using role
+-- bigbang user create myrole.
+--myrole includes with select and other priviligies
+--myrole grant to practice
+--Check that practice user can select books 
+
+--1. admin session
+grant create role to bigbang;
+
+--2. bigbang session
+create role myrole;
+
+grant select,insert on books to myrole;
+
+grant to myrole to practice;
+
+
+
+
